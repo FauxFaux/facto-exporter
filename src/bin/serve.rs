f9c3f6380f3eedab42cde02ebac33a45ac52359e
Chris West (Faux) <git@goeswhere.com>
2023-07-09 20:11:08 +0100

    feat: last api?

--- src/bin/serve.rs
+++ src/bin/serve.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::future::Future;
 use std::sync::Arc;
 use std::{fs, io};
@@ -83,6 +84,7 @@ async fn main() -> Result<()> {
         .route("/metrics/raw", get(metrics_raw))
         .route("/exp/store", post(store))
         .route("/api/query", get(query))
+        .route("/api/last", get(last))
         .with_state(Arc::new(AppState {
             data: Arc::new(tokio::sync::Mutex::new(data)),
             logger: Bunyarr::with_name("handler"),
@@ -143,6 +145,23 @@ async fn metrics_raw(State(state): State<Arc<AppState>>) -> String {
     s
 }
 
+fn split_units(logger: &Bunyarr, units: &str) -> Option<Vec<u32>> {
+    match units
+        .split(',')
+        .map(|s| -> Result<u32> { Ok(s.parse::<u32>().with_context(|| anyhow!("{s:?}"))?) })
+        .collect::<Result<Vec<u32>>>()
+    {
+        Ok(mut units) => {
+            units.sort_unstable();
+            Some(units)
+        }
+        Err(err) => {
+            logger.warn(vars_dbg! { err }, "failed to parse units");
+            None
+        }
+    }
+}
+
 #[derive(serde::Deserialize)]
 struct QueryQuery {
     // number of observations to return, default 30
@@ -160,25 +179,15 @@ async fn query(
     State(state): State<Arc<AppState>>,
     Query(query): Query<QueryQuery>,
 ) -> impl IntoResponse {
-    let mut units = match query
-        .units
-        .split(',')
-        .map(|s| -> Result<u32> { Ok(s.parse::<u32>().with_context(|| anyhow!("{s:?}"))?) })
-        .collect::<Result<Vec<u32>>>()
-    {
-        Ok(units) => units,
-        Err(err) => {
-            state
-                .logger
-                .warn(vars_dbg! { err }, "failed to parse units");
+    let units = match split_units(&state.logger, &query.units) {
+        Some(units) => units,
+        None => {
             return (
                 StatusCode::BAD_REQUEST,
                 Json(json!({ "error": "invalid units" })),
             );
         }
     };
-
-    units.sort_unstable();
 
     let end = query
         .end
@@ -266,6 +275,94 @@ async fn query(
             .collect::<Vec<_>>();
 
         Ok(json!({ "units": units, "deltas": deltas, "statuses": statuses, "times": times }))
+    })
+    .await
+}
+
+#[derive(serde::Deserialize)]
+struct LastQuery {
+    // Vec<u32> csv
+    units: String,
+}
+
+#[derive(serde::Serialize, Default)]
+struct UnitData {
+    produced_change: Option<i64>,
+    last_status: Option<u32>,
+    last_status_change: Option<i64>,
+    previous_status: Option<u32>,
+}
+
+impl UnitData {
+    fn all_complete(&self) -> bool {
+        self.produced_change.is_some()
+            && self.last_status.is_some()
+            && self.previous_status.is_some()
+            && self.last_status_change.is_some()
+    }
+}
+
+#[axum::debug_handler]
+async fn last(
+    State(state): State<Arc<AppState>>,
+    Query(query): Query<LastQuery>,
+) -> impl IntoResponse {
+    let units = match split_units(&state.logger, &query.units) {
+        Some(units) => units,
+        None => {
+            return (
+                StatusCode::BAD_REQUEST,
+                Json(json!({ "error": "invalid units" })),
+            );
+        }
+    };
+
+    okay_or_500(&state.logger, || async {
+        let data = state.data.lock().await;
+        ensure!(!data.inner.is_empty(), "no data");
+
+        let mut changes = HashMap::with_capacity(units.len());
+
+        for unit in &units {
+            let mut unit_data = UnitData::default();
+            let mut produced_previous = None;
+            let mut status_previous = None;
+            for obs in data.inner.iter().rev() {
+                let found = match obs
+                    .inner
+                    .binary_search_by_key(&unit, |crafting| &crafting.unit_number)
+                {
+                    Ok(found) => found,
+                    Err(_) => continue,
+                };
+                let found = &obs.inner[found];
+                if unit_data.produced_change.is_none() {
+                    if produced_previous.is_some()
+                        && produced_previous != Some(found.products_complete)
+                    {
+                        unit_data.produced_change = Some(obs.time.unix_timestamp());
+                    }
+                    produced_previous = Some(found.products_complete);
+                }
+                if unit_data.last_status.is_none() {
+                    unit_data.last_status = Some(found.status);
+                }
+                if unit_data.last_status_change.is_none() {
+                    if status_previous.is_some() && status_previous != Some(found.status) {
+                        unit_data.last_status_change = Some(obs.time.unix_timestamp());
+                        unit_data.previous_status = Some(found.status);
+                    }
+                    status_previous = Some(found.status);
+                }
+
+                if unit_data.all_complete() {
+                    break;
+                }
+            }
+            changes.insert(unit, unit_data);
+        }
+
+        Ok(json!({ "changes": changes }))
     })
     .await
 }
